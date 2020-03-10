d82bcc50b525f0830051797c879ab6f62777b3d9
官维 <guanwei@triumen.com>
2020-03-10 11:54:58 +0800

    vue-vuex start

--- src/pages/news/news.vue
+++ src/pages/news/news.vue
@@ -1,11 +1,18 @@
 <template>
   <div class>
     <p>新闻页</p>
+    <div>
+      <button @click="reduce">-</button>
+      <span>{{$store.state.count}}</span>
+      <button @click="add">+</button>
+    </div>
     <router-view></router-view>
   </div>
 </template>
 
 <script>
+import { mapActions } from "vuex"; // vuex 的 action 高级用法， 映射
+
 export default {
   components: {},
   data() {
@@ -13,16 +20,28 @@ export default {
   },
   // ---------------------组件内部路由守卫
   beforeRouteEnter(to, from, next) {
-    console.log("进来", this);  // 这个获取不到 this
+    console.log("进来", this); // 这个获取不到 this
     next();
   },
   beforeRouteLeave(to, from, next) {
     // 主要用来在一些需要存储信息的页面，错误离开数据丢失
-    console.log("离开", this);  // 这个能获取到 this
+    console.log("离开", this); // 这个能获取到 this
     next();
   },
   mounted() {},
-  methods: {}
+  methods: {
+    // add() {
+    //   // this.$store.dispatch("handelAdd");  // dispatch 操作 action
+    //   this.$store.commit("addCount"); // commit 操作 mutations
+    // },
+    // reduce() {
+    //   this.$store.commit("reduceCount");
+    // },
+    ...mapActions({
+      add: "handelAdd",  // add 方法映射 vuex 的 action 的 handelAdd
+      reduce: "handelReduce"
+    })
+  }
 };
 </script>
 
--- src/store/index.js
+++ src/store/index.js
@@ -5,11 +5,30 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    count: 100,
   },
   mutations: {
+    addCount(state) {
+      state.count++
+    },
+    reduceCount(state) {
+      state.count--
+    }
   },
   actions: {
+    // 通过 commit 操作 mutations
+    handelAdd: ({
+      commit // es6 解构赋值
+    }) => {
+      commit('addCount')
+    },
+    handelReduce: ({
+      commit
+    }) => {
+      commit('reduceCount')
+    }
   },
   modules: {
+
   }
 })
